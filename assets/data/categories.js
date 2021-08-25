bb1e469e1db9e97549e5d0ebb9cd718f37e2638e
thangly <thangly2k1@gmail.com>
2021-08-25 08:32:14 +0700

    Asset Image

---
+++ assets/data/categories.js
@@ -0,0 +1,7 @@
+const categories = [
+  {id: '1', name: 'Quần', image: require('../catergories/pants.png')},
+  {id: '2', name: 'Áo', image: require('../catergories/ao.png')},
+  {id: '3', name: 'Giày', image: require('../catergories/ao.png')},
+  {id: '4', name: 'Túi xách', image: require('../catergories/ao.png')},
+];
+export default categories;
