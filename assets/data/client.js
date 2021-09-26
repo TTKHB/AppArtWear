a1f5cd3d367f2e01c59b21d7ca77cd67645b68ae
thangly <thangly2k1@gmail.com>
2021-09-26 23:40:40 +0700

    client user

--- assets/data/client.js
+++ assets/data/client.js
@@ -2,4 +2,4 @@ import axios from 'axios';
 
 // export default axios.create({baseURL:'http://10.0.3.2:3000'});
 
-export default axios.create({baseURL:'http://192.168.0.108:3000/api/v1/users'});
+export default axios.create({baseURL:'http://10.0.3.2:3000/api/v1/users'});
