86bb77e10903c085478b10e1c427b650a315aad9
thangly <thangly2k1@gmail.com>
2021-11-21 11:50:58 +0700

    Fix cart when null price

--- Screens/Cart/CartScreen.js
+++ Screens/Cart/CartScreen.js
@@ -28,7 +28,12 @@ import DialogCart from '../../components/Cart/DialogCart';
 const CartScreen = ({ navigation }) => {
   const { isLoggedIn, profile } = useLogin();
   const [cartList, setcartList] = useState([]);
-  const itemsPrice = cartList.reduce((a, c) => a + c.amount * c.product_id.gia, 0,);
+  let itemsPrice = 0;
+  cartList.forEach((order) => {
+    if (order) {
+      itemsPrice += order.amount * (order.product_id ? order.product_id.gia : '');
+    }
+  });
 
   const [products, setProducts] = useState([]);
 
@@ -225,11 +230,11 @@ const CartScreen = ({ navigation }) => {
             source={{ uri: item.ThumbImg ? item.ThumbImg : null }}
           />
           <Text style={styles.priceSp}>
-            {item.gia} VNĐ
+            {item.gia ? item.gia.toFixed(3).replace(/\d(?=(\d{3})+\.)/g, '$&.') : ''} VNĐ
           </Text>
           <Text
             style={styles.priceSale}>
-            {item.giacu} VNĐ
+            {item.giacu ? item.giacu.toFixed(3).replace(/\d(?=(\d{3})+\.)/g, '$&.') : ''} VNĐ
           </Text>
         </TouchableOpacity>
       </View>
