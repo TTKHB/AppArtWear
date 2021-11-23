b22ed8c1a4f7845fabb05e6d81df5d2ff90892ba
thangly <thangly2k1@gmail.com>
2021-11-23 16:07:13 +0700

    WaitPayment

--- Screens/User/PurchaseOrder/WaitPayment.js
+++ Screens/User/PurchaseOrder/WaitPayment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -8,219 +8,117 @@ import {
   TouchableOpacity,
   FlatList,
 } from 'react-native';
-export const top = require('../../../assets/images/huyen.jpg');
-export const tt = require('../../../assets/images/protect.jpg');
-const product = [
-  {
-    id: 1,
-    image: top,
-    title: 'MOTF PREMIUM Đầm Thắt lưng Cao thấp màu trơn Giải trí',
-    SL: '1',
-    price: '300.000',
-    date: '24-04-2022',
-  },
-  {
-    id: 2,
-    image: top,
-    title: 'SHEIN PETITE Đầm viền lá sen Xù màu trơn Thanh lịch',
-    SL: '1',
-    price: '300.000',
-    date: '24-04-2022',
-  },
-];
-const WaitPayment = () => {
-  return (
-    <View style={styles.container}>
-      {/* <Image style={styles.images} source={{
-        uri: 'https://www.trangmall.com/Client/upload/News/User_1/2018/12/3/6P2SHv.png',
-      }} />
-      <Text style={styles.welcome}>
-        Chờ thanh toán
-      </Text> */}
+import { styles } from '../../../components/MyOrder/styles/WaitPaymentStyles';
 
-      <FlatList
-        data={product}
-        keyExtractor={item => item.id}
-        renderItem={({item}) => {
-          return (
-            <View style={styles.container2}>
+import axios from 'axios';
+import { useFocusEffect } from '@react-navigation/native';
+import baseURL from '../../../assets/common/baseUrl';
+
+import { useLogin } from '../../../Context/LoginProvider';
+
+export const tt = require('../../../assets/images/protect.jpg');
+
+const WaitPayment = ({ navigation }) => {
+  const { profile } = useLogin();
+  const [orderList, setorderList] = useState([]);
+
+  useFocusEffect(
+    useCallback(() => {
+      axios
+        .get(`${baseURL}orders/get/userorderss/` + profile._id)
+        .then(res => {
+          setorderList(res.data);
+          console.log(res.data)
+        })
+        .catch(error => {
+          console.log('Api call error nha');
+        });
+
+      return () => {
+        setorderList([]);
+      };
+    }, []),
+  );
+
+  const renderCancel = ({ item }) => {
+    return (
+      <View>
+        {item.status == "2" ? (
+          <>
+            <View style={styles.container2} key={item._id}>
               <View style={styles.view1}>
                 <View
-                  style={{
-                    width: 100,
-                    height: 23,
-                    backgroundColor: 'red',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    borderRadius: 7,
-                  }}>
-                  <Text style={{fontWeight: 'bold', color: 'white'}}>
+                  style={styles.HeaderItem}>
+                  <Text style={styles.ArtWearMall}>
                     ArtWear Mall
                   </Text>
                 </View>
-                <Text style={{color: 'black', marginLeft: 10, fontSize: 15}}>
-                  unilevervn_beauty
-                </Text>
-                <TouchableOpacity style={{marginLeft: '18%'}}>
+                <TouchableOpacity >
                   <Text
-                    style={{fontSize: 17, fontWeight: 'bold', color: 'red'}}>
-                    Vận chuyển
+                    style={styles.textCancel}>
+                    Chờ thanh toán
                   </Text>
                 </TouchableOpacity>
               </View>
-              <View style={styles.view2}>
-                <View style={styles.view21}>
-                  <Image
-                    style={{width: '85%', height: '85%'}}
-                    source={item.image}
-                  />
-                </View>
-                <View style={styles.view22}>
-                  <View style={{width: '100%', height: '60%'}}>
-                    <Text style={{fontSize: 18}}>{item.title}</Text>
+              {item.orderitems.map(e => (
+                <View style={styles.view2} key={e._id}>
+                  <View style={styles.view21}>
+                    <Image
+                      style={styles.styleImage}
+                      source={{ uri: e.product ? e.product.ThumbImg : ' ' }}
+                    />
                   </View>
-                  <View
-                    style={{
-                      width: '100%',
-                      height: '40%',
-                      justifyContent: 'space-between',
-                      flexDirection: 'row',
-                      alignItems: 'center',
-                    }}>
-                    <Text style={[styles.textSize, styles.color]}>
-                      7 ngày đổi trả hàng
-                    </Text>
-                    <Text style={[styles.textSize, styles.color]}>
-                      269.000đ
-                    </Text>
+                  <View style={styles.view22}>
+                    <View >
+                      <Text style={{ fontSize: 18 }}>
+                        {e.product ? e.product.ten : ' '}
+                      </Text>
+                    </View>
+                    <View >
+                      <Text style={{ fontSize: 18 }}>
+                        x {e.quantity}
+                      </Text>
+                    </View>
+                    <View >
+                      <Text style={[styles.textSize, styles.color]}>
+                        {e.product ? e.product.gia.toFixed(3).replace(/\d(?=(\d{3})+\.)/g, '$&.') : ' '}  VNĐ
+                      </Text>
+                    </View>
                   </View>
                 </View>
+              ))}
+              <View style={styles.view32}>
+                <Image style={{ width: 23, height: 23, marginRight: 5 }} source={tt} />
+                <Text style={{ fontSize: 18, fontWeight: 'bold' }}>{item.totalFinalPrice.toFixed(3).replace(/\d(?=(\d{3})+\.)/g, '$&.')} VNĐ</Text>
               </View>
-              <View style={styles.view3}>
-                <View style={styles.view31}>
-                  <Text style={{fontSize: 15}}>
-                    Số lượng sản phẩm: {item.SL}
-                  </Text>
+              <View style={styles.viewFooter}>
+                <View >
+                  <Text>Ngày mua hàng:</Text>
+                  <Text>{item.dateOrdered}</Text>
+                  <Text>Dự kiến giao hàng 3-7 ngày</Text>
                 </View>
-                <View style={styles.view32}>
-                  <Image style={{width: 23, height: 23}} source={tt} />
-                  <Text style={{fontSize: 15}}>Thành tiền: {item.price}đ</Text>
-                </View>
-              </View>
-              <View style={styles.view4}>
-                <View style={styles.view31}>
-                  <Text>Sản phẩm được chuyển đi ngày</Text>
-                  <Text>{item.date}</Text>
-                </View>
-                <View style={styles.view321}>
-                  <TouchableOpacity
-                    style={{
-                      width: '80%',
-                      height: '80%',
-                      backgroundColor: 'red',
-                      borderRadius: 5,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}>
-                    <Text style={{color: 'white'}}>Mua lại</Text>
-                  </TouchableOpacity>
-                </View>
+
+                <TouchableOpacity
+                  onPress={() => navigation.navigate('CartNavigator', { screen: 'Cart' })}
+                  style={styles.btnMuaLai}>
+                  <Text style={{ color: 'white' }}>Theo dõi</Text>
+                </TouchableOpacity>
               </View>
             </View>
-          );
-        }}
+          </>
+        ) : null}
+      </View>
+    );
+  }
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={orderList}
+        keyExtractor={item => item._id}
+        renderItem={renderCancel}
       />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#DDDDDD',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  images: {
-    width: 100,
-    height: 100,
-  },
-  container2: {
-    width: '100%',
-    height: 220,
-    backgroundColor: 'white',
-    marginTop: 10,
-    alignItems: 'center',
-  },
-  view1: {
-    backgroundColor: 'white',
-    width: '96%',
-    height: '20%',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  view2: {
-    backgroundColor: 'white',
-    width: '96%',
-    height: '40%',
-    flexDirection: 'row',
-    borderBottomWidth: 0.2,
-  },
-  view3: {
-    width: '96%',
-    height: '20%',
-    borderBottomWidth: 0.2,
-    flexDirection: 'row',
-    borderBottomColor: 'gray',
-  },
-  view4: {
-    width: '96%',
-    height: '20%',
-    flexDirection: 'row',
-  },
-  view21: {
-    width: '25%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  view22: {
-    width: '75%',
-    height: '100%',
-    marginTop: 2,
-  },
-  view31: {
-    width: '55%',
-    height: '100%',
-    backgroundColor: 'white',
-    justifyContent: 'center',
-  },
-  view32: {
-    width: '45%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  view321: {
-    width: '45%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
-  textSize: {
-    fontSize: 16,
-  },
-  color: {
-    color: 'red',
-  },
-});
 
 export default WaitPayment;
