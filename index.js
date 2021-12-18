d3c3215d5bab8c5366eebd410b64a4c9bfd762bf
Bui Trung Bac <huyboken@gmail.com>
2021-12-18 23:09:46 +0700

    fix

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
  */
 
 import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-import LoaderNotification from './components/Home/Loader/LoaderNotification';
-import LoaderNotificationSale from './components/Home/Loader/LoaderNotificationSale';
-AppRegistry.registerComponent(appName, () => LoaderNotificationSale);
+import { name as appName } from './app.json';
+AppRegistry.registerComponent(appName, () => App);
